66931b66da64c3f49b20e54364cfc460f9da722a
Dom Porada <dom@dom.engineering>
2026-05-01 23:04:34 +0200

    Update dependencies

--- prettier.config.ts
+++ prettier.config.ts
@@ -2,26 +2,4 @@ import { defineConfig } from '@standard-config/prettier';
 
 export default defineConfig({
 	plugins: ['@domfiles/prettier-plugin-fish'],
-	overrides: [
-		{
-			files: [
-				/* prettier-ignore */
-				'.zed/keymap.json',
-				'**/zed/keymap.json',
-			],
-			options: {
-				jsonSortOrder: ['context', 'bindings'],
-			},
-		},
-		{
-			files: [
-				/* prettier-ignore */
-				'.zed/settings.json',
-				'**/zed/settings.json',
-			],
-			options: {
-				jsonSortOrder: ['default', '*', 'agent'],
-			},
-		},
-	],
 });
