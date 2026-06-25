50f90cee077ad3d1083bda566680f48f60e26567
Dom Porada <dom@dom.engineering>
2026-06-25 12:09:19 +0200

    Clean up migrations

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -25,20 +25,3 @@ function clone
         git clone $argv
     end
 end
-
-# Clean up Fish universal variables
-function __domfiles_clean_fish_variables
-    for name in (set --names --universal)
-        switch "$name"
-            case "fish_color_*" "fish_pager_color_*"
-                set --erase --universal "$name"
-        end
-    end
-
-    # Fish 4.8+ no longer creates `__fish_initialized`
-    if string match --quiet '4.8.*' "$version"
-        set --erase --universal __fish_initialized
-    end
-
-    true
-end
